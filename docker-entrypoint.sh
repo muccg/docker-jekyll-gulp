873f92642f09ee3bbef33d72ad14d6d2b59ad0d4
aahunter <ahunter@ccg.murdoch.edu.au>
2016-09-23 11:18:31 +0800

    yak-shave update

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -15,18 +15,29 @@ env | grep -iv PASS | sort
 
 if [ "$1" = 'dev' ]; then
     echo "[Run] developing site"
+
     gulp_run
-    cd /app/site && exec bundle exec jekyll serve --host=0.0.0.0
+
+    cd /app/site 
+    bundle 
+
+    exec jekyll serve --host=0.0.0.0
 fi
 
 if [ "$1" = 'releasetarball' ]; then
     echo "[Run] building production tarball"
+
     gulp_run
-    cd /app/site && bundle exec jekyll build
-    cd /app/site/_site && exec tar czf /build/${TARBALL_NAME}-${TARBALL_GIT_TAG}.tar.gz .
+
+    cd /app/site
+    bundle
+    jekyll build
+
+    cd /app/site/_site
+    exec tar czf /build/${TARBALL_NAME}-${TARBALL_GIT_TAG}.tar.gz .
 fi
 
-echo "[RUN]: Builtin command not provided [uwsgi]"
+echo "[RUN]: Builtin command not provided [dev|releasetarball]"
 echo "[RUN]: $@"
 
 exec "$@"
